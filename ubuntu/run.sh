7b96390288150304c0421b19ae00ddb33e33b4ea
ohmybuggg <sa0963278610@gmail.com>
2023-09-05 11:05:45 +0900

    debug for ubuntu gpu used

--- ubuntu/run.sh
+++ ubuntu/run.sh
@@ -1,1 +1,1 @@
-sudo docker run --rm -it --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -v ./my-workspace/:/my-workspace -p 8887:8888 nvflare-pt:latest
+docker run -it --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -v /home/aistubuntu/PoYu/tmp_dataset:/root/datasets -p 8102-8103:8102-8103 pytorch_sound:latest
